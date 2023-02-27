ef3cf4948dc67112cad0986a2172e73a7543fa6b
 Tomek Sułkowski <tomek.sulkowski@gmail.com>
2023-02-27 11:47:28 +0100

    initial structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let projectName = 'demo';
+
+	let gitOptions = [
+		{ label: 'github.com/sulco/', dynamic: true },
+		{ label: 'gitlab.com/sulco/', dynamic: true },
+		{ label: 'just run `git init`', dynamic: false },
+		{ label: 'no git', dynamic: false }
+	];
+
+	let initScripts = [
+		'npm create vite@latest',
+		'npx ng new',
+		'npm init video',
+		'pnpm create next-app --typescript'
+	];
+</script>
+
+<h1>start a new project</h1>
+
+<fieldset>
+	<div class="field">
+		<label for="name">Project name</label>
+		<input id="name" type="text" bind:value={projectName} />
+	</div>
+</fieldset>
+
+<fieldset>
+	<div class="radios">
+		<h2>Git:</h2>
+		{#each gitOptions as option, i}
+			<div class="field">
+				<input type="radio" name="git" id="g{i}" />
+				<label for="g{i}">{option.label}{option.dynamic ? projectName : ''}</label>
+			</div>
+		{/each}
+	</div>
+
+	<div class="radios">
+		<h2>Initialize project with:</h2>
+		{#each initScripts as option, i}
+			<div class="field">
+				<input type="radio" name="script" id="g{i}" />
+				<input type="text" bind:value={option} />
+			</div>
+		{/each}
+	</div>
+</fieldset>
+
+<button>Create</button>
